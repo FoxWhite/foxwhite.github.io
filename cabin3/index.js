6fa87fb5ea7f6078e77a70ab50b8cb6b445c394b
Egor Belov <georgy.belov@revolut.com>
2018-08-10 18:39:08 +0300

    fix music play

--- cabin3/index.js
+++ cabin3/index.js
@@ -7,12 +7,34 @@ setTimeout(() => {
 }, 500)
 
 
-image.onload = () => {
-  document.getElementById('loader').classList.remove('active')
-  document.getElementById('music').play()
-  document.getElementById('bg').style['background-image'] = `url(${image.src})`
-  setTimeout(() => {
-    document.getElementById('bg').style['opacity'] = '1'
-  }, 4)
-  document.getElementsByTagName('main')[0].classList.remove('loading')
+const loading = (() => {
+  let imageLoaded = false
+  let musicLoaded = false
+  return {
+    setImageLoaded: (newVal = true) => imageLoaded = newVal,
+    setMusicLoaded: (newVal = true) => musicLoaded = newVal,
+    isLoaded: () => imageLoaded && musicLoaded
+  }
+})()
+
+const checkLoading = () => {
+  if (loading.isLoaded()) {
+    document.getElementById('loader').classList.remove('active')
+    document.getElementById('music').play()
+    document.getElementById('bg').style['background-image'] = `url(${image.src})`
+    setTimeout(() => {
+      document.getElementById('bg').style['opacity'] = '1'
+    }, 4)
+    document.getElementsByTagName('main')[0].classList.remove('loading')
+  }
 }
+
+image.onload = () => {
+  loading.setImageLoaded()
+  checkLoading()
+}
+
+music.addEventListener('canplaythrough', () => {
+  loading.setMusicLoaded()
+  checkLoading()
+})
